7f261f0a3a4e88c25c32dc4201ad6224934f7ef6
Arly Moura <arlymoura@gmail.com>
2020-05-16 18:03:14 -0300

    add job parser file

---
+++ app/jobs/file_purchases_parser_job.rb
@@ -0,0 +1,42 @@
+class FilePurchasesParserJob < ApplicationJob
+  require 'csv'
+  queue_as :default
+ 
+
+  def perform(file)
+    purchasers = []
+    ActiveRecord::Base.transaction do
+      CSV.foreach(file.path, col_sep: "\t", headers: true, header_converters: :symbol) do |row|
+        content = row.to_hash
+
+        purchaser = parse_purchaser(content[:purchaser_name])
+        merchant = parse_merchant(content[:merchant_name], content[:merchant_address])
+        item = parse_item(content[:item_description], content[:item_price], merchant)
+        purchase = parse_purchase(purchaser, item, content[:purchase_count])
+
+        purchaser.save
+      end
+    end
+  end
+
+  private
+
+  def parse_purchaser(name)
+    Purchaser.find_or_create_by(name: name)
+  end
+
+  def parse_merchant(name, address)
+    Merchant.find_or_create_by(name: name) do |m|
+      m.address = address
+    end
+  end
+
+  def parse_item(description, price, merchant)
+    Item.create(description: description, price: price.to_f, merchant: merchant)
+  end
+
+  def parse_purchase(purchaser, item, quantity)
+    Purchase.create(purchaser: purchaser, item: item, quantity: quantity.to_i)
+  end
+
+end
