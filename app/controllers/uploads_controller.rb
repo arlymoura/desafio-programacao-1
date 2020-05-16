3cda02c02b4b20c11ab05226b1efdcf87757a4ff
Arly Moura <arlymoura@gmail.com>
2020-05-16 17:54:53 -0300

    add controller uploads

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,22 @@
+class UploadsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+  end
+
+  def create
+    parser_file_job
+ 
+    flash[:notice] = "Estamos processando o arquivo de comporas \\o/"
+    redirect_to '/home'
+  end
+
+  private
+
+  def parser_file_job
+    file = params[:file] if params[:file]
+    #FilePurchasesParserJob.perform_later(file) 
+    FilePurchasesParserJob.perform_now(file) 
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :home, only: [:index, :create]
+  resources :home, only: [:index]
+  resources :uploads, only: [:new, :create]
   root :to => "home#index"
 end
